11f85d478ab77cf38d29e8ce6908c7033ef62130
Peter Oyebanji <peter@prob.ca>
2020-10-02 15:23:45 -0600

    reserve2

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const btnTime = document.getElementById('get-time')
 const timeSelect = document.getElementById('time-select')
 
 function dateSelected() {
-    // var x = document.getElementById("myDate").value;
-    // document.getElementById("date-selected").innerHTML = x;
+
     dateSelect.textContent = date.value
     if(appt.value === "") {
       alert('Please Select Time')
@@ -16,13 +15,6 @@ function dateSelected() {
   }
   }
 
-//   const timeSelected = ()=> {
-//     if(appt.value === "") {
-//       alert('Please Select Time')
-//     } else {
-//   timeSelect.textContent = appt.value + ' Hrs'
-//   }
-// }
 
   btnDate.addEventListener('click', dateSelected)
   btnTime.addEventListener('click', timeSelected)
