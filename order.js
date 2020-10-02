3a5fdaaadc354023f7683d6c726cb5158c1de2bc
Danielle Barker <caseybarker86@gmail.com>
2020-10-02 17:52:17 -0600

    toppings images

--- order.js
+++ order.js
@@ -1,7 +1,56 @@
 let pizzaToppings = {
-    bacon: 0,
-    pepperoni: 0,
-    pineapple: 0
+    bacon: {
+        quantity: 0,
+        svg: `<svg class="toppingSvg" height="200" width="200">\
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="20" height="10" stroke="black" stroke-width="1" fill="brown" /> \
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="20" height="10" stroke="black" stroke-width="1" fill="brown" /> \
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="20" height="10" stroke="black" stroke-width="1" fill="brown" /> \
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="20" height="10" stroke="black" stroke-width="1" fill="brown" /> \
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="20" height="10" stroke="black" stroke-width="1" fill="brown" /> \
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="20" height="10" stroke="black" stroke-width="1" fill="brown" /> \
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="20" height="10" stroke="black" stroke-width="1" fill="brown" /> \
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="20" height="10" stroke="black" stroke-width="1" fill="brown" /> \
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="20" height="10" stroke="black" stroke-width="1" fill="brown" /> \
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="20" height="10" stroke="black" stroke-width="1" fill="brown" /> \
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="20" height="10" stroke="black" stroke-width="1" fill="brown" /> \
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="20" height="10" stroke="black" stroke-width="1" fill="brown" /> </svg >`},
+    pineapple: {
+        quantity: 0,
+        svg: `<svg class="toppingSvg" height="200" width="200">\
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="15" height="15" stroke="black" stroke-width="1" fill="yellow" /> \
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="15" height="15" stroke="black" stroke-width="1" fill="yellow" /> \
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="15" height="15" stroke="black" stroke-width="1" fill="yellow" /> \
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="15" height="15" stroke="black" stroke-width="1" fill="yellow" /> \
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="15" height="15" stroke="black" stroke-width="1" fill="yellow" /> \                
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="15" height="15" stroke="black" stroke-width="1" fill="yellow" /> \
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="15" height="15" stroke="black" stroke-width="1" fill="yellow" /> \
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="15" height="15" stroke="black" stroke-width="1" fill="yellow" /> \
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="15" height="15" stroke="black" stroke-width="1" fill="yellow" /> \
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="15" height="15" stroke="black" stroke-width="1" fill="yellow" /> \                
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="15" height="15" stroke="black" stroke-width="1" fill="yellow" /> \
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="15" height="15" stroke="black" stroke-width="1" fill="yellow" /> \
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="15" height="15" stroke="black" stroke-width="1" fill="yellow" /> \
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="15" height="15" stroke="black" stroke-width="1" fill="yellow" /> \
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="15" height="15" stroke="black" stroke-width="1" fill="yellow" /> \                
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="15" height="15" stroke="black" stroke-width="1" fill="yellow" /> \
+                <rect x="${40 + Math.random() * 120}" y="${40 + Math.random() * 120}" width="15" height="15" stroke="black" stroke-width="1" fill="yellow" /> </svg >`
+    },
+    pepperoni: {
+        quantity: 0,
+        svg: `<svg class="toppingSvg" height="200" width="200">\
+                <circle cx="${40 + Math.random() * 120}" cy="${40 + Math.random() * 120}" r="10" stroke="black" stroke- width="1" fill = "red" />\
+                <circle cx="${40 + Math.random() * 120}" cy="${40 + Math.random() * 120}" r="10" stroke="black" stroke-width="1" fill="red" />\
+                <circle cx="${40 + Math.random() * 120}" cy="${40 + Math.random() * 120}" r="10" stroke="black" stroke-width="1" fill="red" />\
+                <circle cx="${40 + Math.random() * 120}" cy="${40 + Math.random() * 120}" r="10" stroke="black" stroke- width="1" fill = "red" />\
+                <circle cx="${40 + Math.random() * 120}" cy="${40 + Math.random() * 120}" r="10" stroke="black" stroke-width="1" fill="red" />\
+                <circle cx="${40 + Math.random() * 120}" cy="${40 + Math.random() * 120}" r="10" stroke="black" stroke-width="1" fill="red" />\
+                <circle cx="${40 + Math.random() * 120}" cy="${40 + Math.random() * 120}" r="10" stroke="black" stroke- width="1" fill = "red" />\
+                <circle cx="${40 + Math.random() * 120}" cy="${40 + Math.random() * 120}" r="10" stroke="black" stroke-width="1" fill="red" />\
+                <circle cx="${40 + Math.random() * 120}" cy="${40 + Math.random() * 120}" r="10" stroke="black" stroke-width="1" fill="red" />\
+                <circle cx="${40 + Math.random() * 120}" cy="${40 + Math.random() * 120}" r="10" stroke="black" stroke- width="1" fill = "red" />\
+                <circle cx="${40 + Math.random() * 120}" cy="${40 + Math.random() * 120}" r="10" stroke="black" stroke-width="1" fill="red" />\
+                <circle cx="${40 + Math.random() * 120}" cy="${40 + Math.random() * 120}" r="10" stroke="black" stroke-width="1" fill="red" />\</svg >`},
+    
 }
 let basePrices = {
     14: 14.99,
@@ -10,27 +59,33 @@ let basePrices = {
     20: 24.99
 }
 function addTopping(toppingName) {
-    pizzaToppings[toppingName]++;
+    pizzaToppings[toppingName].quantity++;
     
     updateOrder();
 }
 
 function removeTopping(toppingName) {
-    pizzaToppings[toppingName]--;
+    pizzaToppings[toppingName].quantity--;
     
     updateOrder();
 }
 
 function generatePizzaPicture() {
-    let pizzaHtml = JSON.stringify(pizzaToppings);
-    $("#pizza").html(pizzaHtml);
+    let toppingHtml = '<svg class="pizzaSvg" height="200" width="200">\
+        <circle cx="100" cy="100" r="100" stroke="brown" stroke-width="3" fill="beige" />\
+        </svg>;'
+    for (topping in pizzaToppings) {
+        if (pizzaToppings[topping].quantity > 0) {
+            toppingHtml += pizzaToppings[topping].svg;
+           
+        }
+        
+    }
+    $("#toppings").html(toppingHtml);
 }
 
 function updateOrder() {
     generatePizzaPicture();
-    let size = $("#pizzasize").val();
-    let cost = basePrices[size] + Object.values(pizzaToppings)
-        .reduce((total, cost) => total += cost * size/20, 0);
-    let costHtml = "$" + cost;
-    $("#pizzaCost").html(costHtml);
+    
+   
 }
