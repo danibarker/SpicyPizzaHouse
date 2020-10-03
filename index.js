ec0fdfb6a40a4f9acec1b48ac53c80549730bbce
Peter Oyebanji <peter@prob.ca>
2020-10-02 19:54:25 -0600

    fixed input

--- index.js
+++ index.js
@@ -4,17 +4,26 @@ const dateSelect= document.getElementById("date-select")
 const appt = document.getElementById('appt')
 const btnTime = document.getElementById('get-time')
 const timeSelect = document.getElementById('time-select')
+const fname = document.getElementById('fname')
+const lname = document.getElementById('lname')
+const number = document.getElementById('number')
+const email = document.getElementById('email')
+const guest = document.getElementById('guest')
+const fulName = document.getElementById('full-name')
+
 
 function dateSelected() {
 
     dateSelect.textContent = date.value
-    if(appt.value === "") {
-      alert('Please Select Time')
+    if(appt.value === "" ||
+    fname.value === '' ||
+    number.value ==='') {
+      alert('Please Select Time, Enter Name or Phone Number')
     } else {
-  timeSelect.textContent = appt.value + ' Hrs'
+  timeSelect.textContent = appt.value + ' Hrs';
+  fulName.textContent = fname.value + " " + lname.value
   }
   }
 
 
   btnDate.addEventListener('click', dateSelected)
-  btnTime.addEventListener('click', timeSelected)
