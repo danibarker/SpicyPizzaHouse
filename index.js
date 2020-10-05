bc281684c680614667508af46a8a2bffbb7c767a
danibarker <caseybarker86@gmail.com>
2020-10-05 10:26:28 -0600

    made formatting consister, added prettier file, deleted unused images

--- index.js
+++ index.js
@@ -1,37 +1,27 @@
-const btnDate = document.getElementById('get-date')
-const date = document.getElementById("myDate")
-const dateSelect= document.getElementById("date-select")
-const appt = document.getElementById('appt')
-const btnTime = document.getElementById('get-time')
-const timeSelect = document.getElementById('time-select')
-const fname = document.getElementById('fname')
-const lname = document.getElementById('lname')
-const number = document.getElementById('number')
-const email = document.getElementById('email')
-const guest = document.getElementById('guest')
-const fulName = document.getElementById('full-name')
-const thanks = document.getElementById('thanks')
-
+const btnDate = document.getElementById("get-date");
+const date = document.getElementById("myDate");
+const dateSelect = document.getElementById("date-select");
+const appt = document.getElementById("appt");
+const btnTime = document.getElementById("get-time");
+const timeSelect = document.getElementById("time-select");
+const fname = document.getElementById("fname");
+const lname = document.getElementById("lname");
+const number = document.getElementById("number");
+const email = document.getElementById("email");
+const guest = document.getElementById("guest");
+const fulName = document.getElementById("full-name");
+const thanks = document.getElementById("thanks");
 
 function dateSelected() {
-
-    
-    if(appt.value === "" ||
-    fname.value === '' ||
-    number.value ==='') {
-      alert('Please Select Time, Enter Name and Phone Number')
+    if (appt.value === "" || fname.value === "" || number.value === "") {
+        alert("Please Select Time, Enter Name and Phone Number");
     } else {
- 
-        window.open("#thanks", "_self")
-        $('#reserve').removeClass('currentSection')
-        $('#thanks').addClass('currentSection')
-      fulName.textContent = `Thank you, ${fname.value}, for your reservation`
-      dateSelect.textContent = ` See you on ${date.value} at ${appt.value} Hrs` 
-      
-    
- 
-  }
-  }
+        window.open("#thanks", "_self");
+        $("#reserve").removeClass("currentSection");
+        $("#thanks").addClass("currentSection");
+        fulName.textContent = `Thank you, ${fname.value}, for your reservation`;
+        dateSelect.textContent = ` See you on ${date.value} at ${appt.value} Hrs`;
+    }
+}
 
-
-  btnDate.addEventListener('click', dateSelected)
+btnDate.addEventListener("click", dateSelected);
--- order.js
+++ order.js
@@ -1,18 +1,17 @@
-$('html, body').css({
-    overflow: 'hidden',
-    height: '100%'
+$("html, body").css({
+    overflow: "hidden",
+    height: "100%",
 });
-$('a').click( function () {
-    $('.navbar-collapse').collapse('hide');
-    $("section").each((i, obj) => $(obj).removeClass('currentSection'));
-    $($(this).attr('href')).addClass('currentSection')
+$("a").click(function () {
+    $(".navbar-collapse").collapse("hide");
+    $("section").each((i, obj) => $(obj).removeClass("currentSection"));
+    $($(this).attr("href")).addClass("currentSection");
 });
-
 
 let orderItem = {
     item: null,
-    price: 0
-}
+    price: 0,
+};
 let orderCost = 0;
 let size = 14;
 let cart = [];
@@ -22,40 +21,39 @@ let itemType;
 let pizzaToppings = {
     bacon: {
         quantity: 0,
-        svg: toppingImages.bacon
+        svg: toppingImages.bacon,
     },
     pepperoni: {
         quantity: 0,
-        svg: toppingImages.pepperoni
+        svg: toppingImages.pepperoni,
     },
     pineapple: {
         quantity: 0,
-        svg: toppingImages.pineapple
+        svg: toppingImages.pineapple,
     },
     hotPepper: {
         quantity: 0,
         svg: toppingImages.hotPepper,
     },
-}
+};
 
 let basePrices = {
     14: 14.99,
     16: 16.99,
     18: 20.99,
-    20: 24.99
-}
+    20: 24.99,
+};
 let wingbasePrices = {
     8: 8.99,
     12: 12.99,
     18: 20.99,
-    30: 24.99
-}
+    30: 24.99,
+};
 let drinkBasePrices = {
-    '355ml can': 1.59,
-    '4 cans': 2.88,
-    '2L bottle': 4.25
-
-}
+    "355ml can": 1.59,
+    "4 cans": 2.88,
+    "2L bottle": 4.25,
+};
 function addTopping(toppingName) {
     pizzaToppings[toppingName].quantity++;
     updateOrder();
@@ -70,22 +68,21 @@ function removeTopping(toppingName) {
 }
 
 function generatePizzaPicture() {
-    let toppingHtml = '<svg class="pizzaSvg" height="210" width="210">\
+    let toppingHtml =
+        '<svg class="pizzaSvg" height="210" width="210">\
         <circle cx="105" cy="105" r="100" stroke="#b5651d" stroke-width="12" fill="beige" />\
-        </svg>;'
+        </svg>;';
     for (topping in pizzaToppings) {
         if (pizzaToppings[topping].quantity > 0) {
             toppingHtml += pizzaToppings[topping].svg;
-           
         }
-        
     }
     $("#toppings").html(toppingHtml);
 }
 
 function updateOrder() {
-    let orderHtml = '';
-    if (itemType === 'pizza') {
+    let orderHtml = "";
+    if (itemType === "pizza") {
         generatePizzaPicture();
         size = $("#pizzasize").val();
         orderHtml = `<div class='row justify-content-between'><div class='col lineItem'>${size}" pizza </div><div class='col lineCost'> ${basePrices[size]}</div></div><br />`;
@@ -96,65 +93,59 @@ function updateOrder() {
             let lineMessage = `<div class='row justify-content-between'><div class='col lineItem'>${topping} x ${quantity} </div><div class='col lineCost'> $${quantity}.00</div></div>`;
             if (quantity > 0) {
                 orderHtml += lineMessage;
-
             }
         }
         orderCost = (toppingsCost + basePrices[size]).toFixed(2);
-        
-    } else if (itemType === 'wings') {
+    } else if (itemType === "wings") {
         size = $("#wingQuantity").val();
         wingFlavor = $("#wingFlavor").val();
         orderHtml = `<div class='row justify-content-between'><div class='col lineItem'>${size} wings </div><div class='col lineCost'> ${wingbasePrices[size]}</div></div>`;
 
-        orderCost = (wingbasePrices[size]).toFixed(2);
-        
+        orderCost = wingbasePrices[size].toFixed(2);
     } else {
-        size = $('#drinkSize').val();
-        drinkType = $('#drinkType').val();
+        size = $("#drinkSize").val();
+        drinkType = $("#drinkType").val();
         orderHtml = `<div class='row justify-content-between'><div class='col lineItem'>${size} </div><div class='col lineCost'> ${drinkBasePrices[size]}</div></div>`;
-        orderCost = (drinkBasePrices[size]).toFixed(2);
-        
+        orderCost = drinkBasePrices[size].toFixed(2);
     }
-    orderHtml += `<div class='row mt-2 justify-content-between border-top border-dark'><div class='col lineItem'>Total </div><div class='col lineCost'> $${orderCost}</div></div>`
-    orderHtml += "<div class='row pt-3 justify-content-between' ><button id='addToCart' onclick='confirmation()' class='btn btn-primary'>Add to Cart</button></div>"
+    orderHtml += `<div class='row mt-2 justify-content-between border-top border-dark'><div class='col lineItem'>Total </div><div class='col lineCost'> $${orderCost}</div></div>`;
+    orderHtml +=
+        "<div class='row pt-3 justify-content-between' ><button id='addToCart' onclick='confirmation()' class='btn btn-primary'>Add to Cart</button></div>";
     $("#pizzaCost").html(orderHtml);
-    
-    
-   
 }
 function confirmation() {
-    let successHtml = 
-        `<div class="alert success" onclick="addToCart()">
+    let successHtml = `<div class="alert success" onclick="addToCart()">
             <span class="closebtn" >Continue</span>
             Success! Item added to cart<br />
         </div>`;
-    $('#pizzaCost').append(successHtml);
-    
+    $("#pizzaCost").append(successHtml);
 }
 function addToCart() {
     orderItem.size = size;
-    if (itemType === 'pizza') {
+    if (itemType === "pizza") {
         orderItem.item = Object.keys(pizzaToppings).map(function (topping) {
             let item = {
                 toppingType: topping,
-                quantity: pizzaToppings[topping].quantity
-            }
+                quantity: pizzaToppings[topping].quantity,
+            };
             return item;
-        })
-    } else if (itemType === 'wings') {
+        });
+    } else if (itemType === "wings") {
         orderItem.item = wingFlavor;
     } else {
         orderItem.item = drinkType;
     }
-    
+
     orderItem.price = orderCost;
     cart.push({ ...orderItem });
     showMenu();
 }
 
 function showMenu() {
-    Object.keys(pizzaToppings).forEach((topping) => pizzaToppings[topping].quantity = 0);
-    $('#order').html(`<h2>Order</h2>
+    Object.keys(pizzaToppings).forEach(
+        (topping) => (pizzaToppings[topping].quantity = 0)
+    );
+    $("#order").html(`<h2>Order</h2>
         <div id="card-group" class="row justify-content-center">
             <div class="col-lg-4 center">
                 <div onclick="showPizza()" class="card">
@@ -184,7 +175,8 @@ function showMenu() {
 }
 function showPizza() {
     itemType = "pizza";
-    $('#order').html(`<button id='goBack' onclick='showMenu()' class='btn btn-secondary'>Back to Menu</button>
+    $("#order")
+        .html(`<button id='goBack' onclick='showMenu()' class='btn btn-secondary'>Back to Menu</button>
         <h2>Make a Pizza</h2>
         <div class="row">
           <div class="col-3">
@@ -200,24 +192,24 @@ function showPizza() {
             </select>
             <h4>Toppings</h4>
             <div>
-             
+
               Pepperoni <br>
               <button class='btn btn-primary btn-sm' onclick="removeTopping('pepperoni')">Less</button>
               <button class='btn btn-primary btn-sm' onclick="addTopping('pepperoni')">More</button>
             </div>
             <div>
-             
+
               Hot Pepper <br>
               <button class='btn btn-primary btn-sm' onclick="removeTopping('hotPepper')">Less</button>
               <button class='btn btn-primary btn-sm' onclick="addTopping('hotPepper')">More</button>
             </div>
-            <div> 
+            <div>
               Pineapple <br>
               <button class='btn btn-primary btn-sm' onclick="removeTopping('pineapple')">Less</button>
               <button class='btn btn-primary btn-sm' onclick="addTopping('pineapple')">More</button>
             </div>
             <div>
-             
+
               Bacon <br>
               <button class='btn btn-primary btn-sm' onclick="removeTopping('bacon')">Less</button>
               <button class='btn btn-primary btn-sm' onclick="addTopping('bacon')">More</button>
@@ -232,10 +224,10 @@ function showPizza() {
                         <circle cx="105" cy="105" r="100" stroke="#b5651d" stroke-width="12" fill="beige" />
                     </svg>
                 </div>
-              
-              
+
+
             </div>
-            
+
           </div>
           <div class="col-3">
             <h3>Make it!</h3>
@@ -247,7 +239,8 @@ function showPizza() {
 
 function showWings() {
     itemType = "wings";
-    $('#order').html(`<button id='goBack' onclick='showMenu()' class='btn btn-secondary'>Back to Menu</button>
+    $("#order")
+        .html(`<button id='goBack' onclick='showMenu()' class='btn btn-secondary'>Back to Menu</button>
         <h2>Get Wings</h2>
         <div class="row">
           <div class="col-5">
@@ -282,7 +275,8 @@ function showWings() {
 
 function showDrinks() {
     itemType = "drinks";
-    $('#order').html(`<button id='goBack' onclick='showMenu()' class='btn btn-secondary'>Back to Menu</button>
+    $("#order")
+        .html(`<button id='goBack' onclick='showMenu()' class='btn btn-secondary'>Back to Menu</button>
         <h2>Get Drinks</h2>
         <div class="row">
           <div class="col-5">
@@ -316,26 +310,34 @@ function showDrinks() {
 
 $("#cartlink").click(function () {
     let total = cart.reduce(function (subtotal, itemPrice) {
-        subtotal += +itemPrice.price
+        subtotal += +itemPrice.price;
         return subtotal;
     }, 3);
     total *= 1.05;
-    let cartHtml = '<h2>Cart</h2><div class="container"><div class="row"><div class="col subtotal"><h3>Your Order</h3>';
+    let cartHtml =
+        '<h2>Cart</h2><div class="container"><div class="row"><div class="col subtotal"><h3>Your Order</h3>';
     for (item of cart) {
-        if (typeof item.item == 'object') {
+        if (typeof item.item == "object") {
             let itemToppings = item.item
                 .filter((topping) => topping.quantity > 0)
-                .reduce((display, curTopping) => display += `${curTopping.toppingType} x ${curTopping.quantity}, `, '');
-            cartHtml += `<div>${item.size}" Pizza - ${item.price}</div>${itemToppings}<br /><br />`
+                .reduce(
+                    (display, curTopping) =>
+                        (display += `${curTopping.toppingType} x ${curTopping.quantity}, `),
+                    ""
+                );
+            cartHtml += `<div>${item.size}" Pizza - ${item.price}</div>${itemToppings}<br /><br />`;
         } else if (!isNaN(item.size)) {
-
             cartHtml += `<div>${item.size} ${item.item} wings - ${item.price}</div><br>`;
         } else {
             cartHtml += `<div>${item.size} ${item.item} - ${item.price}</div><br>`;
         }
     }
-    cartHtml += `<div>Delivery cost - $3.00</div><br /><div>Total - $${total.toFixed(2)}</div>`
-    cartHtml += '</div><div class="col"><h3>Delivery Information</h3>' +`<div class="container mt-3">
+    cartHtml += `<div>Delivery cost - $3.00</div><br /><div>Total - $${total.toFixed(
+        2
+    )}</div>`;
+    cartHtml +=
+        '</div><div class="col"><h3>Delivery Information</h3>' +
+        `<div class="container mt-3">
                     <form action="action_page.php">
 
                         <label for="cartname">Name</label><br />
@@ -352,7 +354,6 @@ $("#cartlink").click(function () {
                         <input class="btn-sm btn btn-primary"type="submit" value="Submit">
 
                     </form>
-                </div></div></div>`
-    $('#cart').html(cartHtml);
+                </div></div></div>`;
+    $("#cart").html(cartHtml);
 });
-
